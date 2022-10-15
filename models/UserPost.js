9356ae2041db7e92b6514a2a14d1646e6c131be6
Alex <alexmore080@gmail.com>
2022-10-15 10:48:39 -0500

    userpost model created

---
+++ models/UserPost.js
@@ -0,0 +1,42 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserPost extends Model {}
+
+UserPost.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: Datatypes.STRING,
+            allowNull: false,
+          },
+          post: {
+            type: Datatypes.STRING,
+            allowNull: false,
+          },
+          date_created: {
+            type: Datatypes.DATE,
+            allowNull: false,
+            defaultValue: Datatypes.NOW,
+          },
+          user_id: {
+            type: Datatypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+          },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'userpost',
+    }
+)
