9254fd4918e5176b073ca4bb82b2e803a00e41a3
Alex <alexmore080@gmail.com>
2022-11-06 13:26:46 -0600

    seed js created to seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,24 @@
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const { User, UserPost } = require("../models");
 
-const userData = require('./userData.json');
+const userData = require("./userData.json");
+const userPost = require("./userPost.json");
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for(const post of userPost) {
+        await UserPost.create({
+            ...post,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        })
+    }
+    process.exit(0);
+}
+
+seedDatabase();
